ec1a074f717dd90a57b12ca47c86e774c56102d3
Pier-Olivier Prevost <pprevost20@gmail.com>
2020-11-04 10:22:48 -0500

    ajout de la class ProduitDAOCSV et de l'interface IProduitDAO

---
+++ Travail/Travail/Donnees/IProduitDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail
+{
+    interface IProduitDAO
+    {
+        void Ajouter(Produit produit);
+        IList<Produit> GetAll();
+        void Modifier(Produit produit);
+        Produit Obtenir(int numero);
+    }
+}
