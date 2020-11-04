7bba986fda944cc0604d7fbcbbc5cd0d35ef3451
DereckBeaudoinRSX <62123542+DereckBeaudoinRSX@users.noreply.github.com>
2020-11-04 10:08:14 -0500

    Ajout de stock dans Panier
    En cours

--- Travail/Travail/Panier.cs
+++ Travail/Travail/Panier.cs
@@ -8,18 +8,44 @@ namespace Travail
 {
 	public class Panier
 	{
+
 		public List<Produit> Cart;
 
-		public void AfficherPanier()
+		public List<string> AfficherPanier()
 		{
+			return ToString(Cart);
+		}
+		
+		public double GetPrixTotal()
+		{
+			return CalculTotal();
+		}
 
+		public double GetPrixProduit(Produit item)
+		{
+			 
 		}
 
 
 
-		private void ToString()
+		private double CalculTotal()
 		{
+			double Resultat = 0; 
+			foreach (var item in Cart)
+			{
+				Resultat = Resultat + (item.Prix * item.Quantite);  
+			}
+			return Math.Round(Resultat, 2); 
+		}
 
+		private List<string> ToString(List<Produit> Cart)
+		{
+			List<string> Resultat = new List<string>(); 
+			foreach (var item in Cart)
+			{
+				Resultat.Add(item.Nom + " " +  item.Prix + "$ " + item.Quantite); 
+			}
+			return Resultat;
 		} 
 
 
