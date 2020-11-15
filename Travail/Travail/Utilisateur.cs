07379d97158c139097620d89da14bc82a8c83ad4
jeremy-PC\jeremy <jbernard3598@gmail.com>
2020-11-15 18:53:34 -0500

    créer class utilisateur

---
+++ Travail/Travail/Utilisateur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail
+{
+    class Utilisateur
+    {
+        public string Nom { get; }
+        public string Role { get; }
+        public decimal Provenance { get; }
+
+        public Utilisateur(string nom, string role, decimal provenance)
+        {
+            Nom = nom;
+            Role = role;
+            Provenance = provenance;
+        }
+    }
+}
